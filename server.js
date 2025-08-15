bc1630b13b67c4bcab09d77779e8219c443e9423
sindric98@ <sindric.loko@stu.ucc.edu.gh>
2025-08-15 18:38:43 +0000

    Fix server to serve files from root directory

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// This will serve your index.html and main.html files
+// This will serve all files (like index.html and main.html) from the root directory
+// This is the correct setup for your current file structure.
 app.use(express.static(path.join(__dirname, '')));
 
 app.listen(PORT, () => {
